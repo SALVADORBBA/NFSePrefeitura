05f71bd5bac713acdc0009b137658ca4e743c8cb
SALVADORBBA <salvadorbba@gmail.com>
2026-01-18 08:10:27 -0300

    Ajuste

--- src/PortoSeguroSigner.php
+++ src/PortoSeguroSigner.php
@@ -51,7 +51,8 @@ class PortoSeguroSigner
                 'transformAlgorithm' => [
                     'http://www.w3.org/2000/09/xmldsig#enveloped-signature',
                     'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
-                ]
+                ],
+                'signatureNode' => 'InfDeclaracaoPrestacaoServico' // Força a assinatura dentro deste nó
             ]
         );
 
